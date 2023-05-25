f7a8d365b7e91a7bcbd71f858986c8ce63e5795f
Nghia Nguyen <nnnghia98@gmail.com>
2023-05-25 14:49:53 +0700

    done header and footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./styles.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="content">© 2023 Nghia Nguyen All Rights Reserved</div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,19 @@ import React from "react";
 import "./styles.scss";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="content">
+        <picture className="logo">portfolio</picture>
+        <div className="navbar-tabs">
+          <div>about</div>
+          <div>experiences</div>
+          <div>projects</div>
+          <div>hobbies</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
